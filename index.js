f1e4f7563025042e635a6d57ebc1051e4268ec5a
RuthRojas <ruro122020@gmail.com>
2023-07-08 17:19:02 -0600

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // Your code here
+
+
+const dodger = document.getElementById('dodger');
+
+document.addEventListener("keydown", function(event){
+  if(event.key === "ArrowLeft"){
+    moveDodgerLeft()
+  }else if(event.key === "ArrowRight"){
+    moveDodgerRight()
+  }
+})
+
+function moveDodgerLeft(){
+ const leftNumber = dodger.style.left.replace('px', '');
+ const left = parseInt(leftNumber, 10)
+
+ if(left > 0){
+  dodger.style.left = `${left - 1}px`;
+
+ }
+}
+
+
+function moveDodgerRight(){
+  const rightNumber = dodger.style.left.replace('px','')
+  const right = parseInt(rightNumber, 10)
+
+  if(right > 0){
+    dodger.style.left = `${right + 1}px`
+  }
+}
